737ce242a9d3f2f09cc9abb0b4ea259a3ce207ee
TheSaltyNewfie <tobydodge65@gmail.com>
2024-06-20 12:24:22 -0230

    Created a category drop down, currently renders twice

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -1,13 +1,26 @@
 //home page
 
 import { useNavigate } from "react-router-dom"
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 export default function RecipeList (props) {
+
+  const [categoryList, setCategoryList] = useState([])
 
   let navigate = useNavigate()
   const linkToExpandedItem = (item) => {
     navigate(`${item.idMeal}`)
   }
+
+  const getCategories = async () => {
+    const response = await axios.get('https://www.themealdb.com/api/json/v1/1/categories.php')
+    response.data.categories.forEach(category => {setCategoryList((prevCategoryList) => [...prevCategoryList, category.strCategory])})
+  }
+
+  useEffect(() => {
+    getCategories()
+  }, [])
 
   // console.log(`Array on list page is ${props.recipeArray}`)
 
@@ -26,6 +39,16 @@ export default function RecipeList (props) {
           required
         />
         <button>Find recipe</button>
+      </form>
+
+      <form onSubmit={props.handleDropdown}>
+        <select name="categories" id="categories">
+          {
+            categoryList.map((category) =>(
+              <option>{category}</option>
+            ))
+          }
+        </select>
       </form>
 
       {
